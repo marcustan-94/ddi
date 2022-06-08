4f837fd24d95d175247f6d50726a0bf7f278d39a
Xin Er Chong <chongxe1991@gmail.com>
2022-06-08 20:19:56 +0800

    cxe modified cxe_feat_eng.py to class functions for getting engineered features

--- cxe_feat_eng.py
+++ cxe_feat_eng.py
@@ -1,141 +1,54 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #importing all needed modules
 import pandas as pd
 from mordred import Calculator, descriptors
 from rdkit import Chem
 
-
-# In[2]:
-
-
-#opening twosides file as df
-twosides_df = pd.read_csv("twosides.csv").drop(columns = "Unnamed: 0")
-
-
-# In[3]:
-
-
-#getting the list for drug1
-drug_list_for_drug_1 = list(twosides_df["Drug1"].unique())
-
-
-# In[4]:
-
-
-#initiating mordred calculator
-calc = Calculator(descriptors, ignore_3D=True)
-
-
-# In[5]:
-
-
-#building drug features for drug1
-mols_for_drug_1 = [Chem.MolFromSmiles(item) for item in drug_list_for_drug_1]
-drug1_features = calc.pandas(mols_for_drug_1)
-
-
-# In[6]:
-
-
-#drug1_features dataframe lacks the SMILE for drug1 itself so add in the SMILE
-drug1_features["Drug1"] = drug_list_for_drug_1
-
-
-# In[7]:
-
-
-#shifting SMILE structure for drug1 to the front so it is easier to visualize
-drug1_features = drug1_features.set_index("Drug1").reset_index()
-
-
-# In[8]:
-
-
-#building the list of columns to drop by selecting dtypes = "object", as we only want the float/int dtypes
-#the list doesn't include "Drug1" column as we need the SMILE structures for merging
-drug1_features_object_list = list(drug1_features.select_dtypes("object").keys()[1:])
-
-
-# In[9]:
-
-
-#dropping the columns where dtypes = "objects", except the "Drug1" column where it contains the SMILE structure
-drug1_features = drug1_features.drop(columns = drug1_features_object_list)
-
-
-# In[10]:
-
-
-#visualizing built drug1 features
-drug1_features
-
-
-# In[11]:
-
-
-#converting the file to csv
-drug1_features.to_csv("cxe_feat_eng_drug1_droppedcolumns.csv")
-
-
-# In[12]:
-
-
-#getting the list for drug2
-drug_list_for_drug_2 = list(twosides_df["Drug2"].unique())
-
-
-# In[13]:
-
-
-#building drug features for drug2
-mols_for_drug_2 = [Chem.MolFromSmiles(item) for item in drug_list_for_drug_2]
-drug2_features = calc.pandas(mols_for_drug_2)
-
-
-# In[14]:
-
-
-#drug2_features dataframe lacks the SMILE for drug1 itself so add in the SMILE
-drug2_features["Drug2"] = drug_list_for_drug_2
-
-
-# In[15]:
-
-
-#shifting SMILE structure for drug1 to the front so it is easier to visualize
-drug2_features = drug2_features.set_index("Drug2").reset_index()
-
-
-# In[16]:
-
-
-#building the list of columns to drop by selecting dtypes = "object", as we only want the float/int dtypes
-#the list doesn't include "Drug1" column as we need the SMILE structures for merging
-drug2_features_object_list = list(drug2_features.select_dtypes("object").keys()[1:])
-
-
-# In[17]:
-
-
-#dropping the columns where dtypes = "objects", except the "Drug1" column where it contains the SMILE structure
-drug2_features = drug2_features.drop(columns = drug2_features_object_list)
-
-
-# In[18]:
-
-
-#visualizing built drug1 features
-drug2_features
-
-
-# In[19]:
-
-
-#converting the file to csv
-drug2_features.to_csv("cxe_feat_eng_drug2_droppedcolumns.csv")
-
+class RawData:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def get_data(self):
+        '''opening twosides file as df'''
+        twosides_df = pd.read_csv(self.file_name).drop(columns = "Unnamed: 0")
+        return twosides_df
+
+    def get_list_for_drug(self, Drug_n):
+        '''getting the list for Drug_n'''
+        '''Drug_n should be in this format: Drug1, or Drug2.
+        eg self.get_list_for_drug(Drug1)'''
+        twosides_df = self.get_data()
+        drug_list_for_drug = list(twosides_df[f"{Drug_n}"].unique())
+        return drug_list_for_drug
+
+    def build_features(self, Drug_n):
+        '''initiating mordred calculator'''
+        calc = Calculator(descriptors, ignore_3D=True)
+        '''gettiing drug list'''
+        drug_list_for_drug = self.get_list_for_drug(Drug_n)
+        '''getting molecular data from items in drug_list_for_drug'''
+        mols = [Chem.MolFromSmiles(item) for item in drug_list_for_drug]
+        '''building pandas dataframe'''
+        drug_features = calc.pandas(mols)
+        '''drug_features dataframe lacks the SMILE for Drug_n itself so add in
+        the SMILE'''
+        drug_features[f"{Drug_n}"] = drug_list_for_drug
+        '''shifting SMILE structure for Drug_n to the front so it is easier to
+        visualize'''
+        drug_features = drug_features.set_index(f"{Drug_n}").reset_index()
+        '''building the list of columns to drop by selecting dtypes = "object",
+        as we only want the float/int dtypes'''
+        '''the list doesn't include "Drug_n" column as we need the SMILE
+        structures for merging'''
+        drug_features_object_list = list(drug_features.
+                                          select_dtypes("object").keys()[1:])
+        '''dropping the columns where dtypes = "objects", except the "Drug_n"
+        column where it contains the SMILE structure'''
+        drug_features = drug_features.drop(columns =
+                                             drug_features_object_list)
+        return drug_features
+
+    def convert_to_csv(self, Drug_n, csv_name):
+        '''converting the file to csv'''
+        drug_features_df = self.build_features(Drug_n)
+        drug_features_df.to_csv(csv_name)
+        return "Conversion successful"
